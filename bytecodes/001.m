553ca05139929b764891d99f0e2f9fad45ecd599
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-28 12:21:16 -0700

    test

---
+++ bytecodes/001.m
@@ -0,0 +1,17 @@
+push 0 push 0 onto the stack
+push 1 push 1 onto the stack
+
+push 2
+   push 3
+                         pall
+
+
+			                 
+
+push 4
+
+        push 5
+	            push 6
+
+
+pall this is the end of our program. monty is awesome!
