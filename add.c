83856749241b37ca9631694fd7d9d8e5ca47291c
Daniel Berihun <danielshitaye10@gmail.com>
2024-03-31 05:12:19 -0700

    add nop

--- add.c
+++ add.c
@@ -14,7 +14,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (temp == NULL || temp->next->next == NULL)
 	{
-		dprintf(2, "L%d: cant't add, stack too short\n", line_number);
+		dprintf(2, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
